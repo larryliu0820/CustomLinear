a6c9631bc00ba9af6d948490ac704c6c08866801
Mengwei Liu <larryliu@fb.com>
2024-05-10 09:24:45 -0700

    Initial Commit

---
+++ CustomLinear/CustomLinear.h
@@ -0,0 +1,18 @@
+//
+//  CustomLinear.h
+//  CustomLinear
+//
+//  Created by Mengwei Liu on 5/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomLinear.
+FOUNDATION_EXPORT double CustomLinearVersionNumber;
+
+//! Project version string for CustomLinear.
+FOUNDATION_EXPORT const unsigned char CustomLinearVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomLinear/PublicHeader.h>
+
+
